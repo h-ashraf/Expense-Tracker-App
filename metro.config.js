2badcb5aaf4acf8cfb838a1a66bc79705f31912c
Hamad Ashraf <81376919+h-ashraf@users.noreply.github.com>
2024-02-28 22:21:02 +0000

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+    const defaultConfig = await getDefaultConfig();
+    return {
+        resolver: {
+        assetExts: [...defaultConfig.resolver.assetExts, 'svg'],
+        },
+        transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        },
+    };
+})();
